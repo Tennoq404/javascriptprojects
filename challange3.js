c0a5c40fd5ce825b69b9d8fd8100e417665a78bc
teenoq404 <teenoqnanchin@gmail.com>
2023-11-28 19:21:06 +0100

    js beginner projects

---
+++ challange3.js
@@ -0,0 +1,12 @@
+dolphinsAve = (96 + 108 + 890)/3
+koalasAve = (88 + 91 + 123)/3
+if (dolphinsAve  > koalasAve) {
+    console.log('dolphins ars the winners')
+} else if(
+    (koalasAve > dolphinsAve) 
+)
+{
+    console.log('koaslas are the winners')
+} else (
+    console.log('there is a draw')
+)
